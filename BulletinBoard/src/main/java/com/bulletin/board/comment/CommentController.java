f459d753332b45095b692b419d8ba5a2089428fa
hjimin027 <hjimin027@gmail.com>
2025-08-11 20:25:00 +0900

    UPDATE comment from post detail page

--- BulletinBoard/src/main/java/com/bulletin/board/comment/CommentController.java
+++ BulletinBoard/src/main/java/com/bulletin/board/comment/CommentController.java
@@ -1,5 +1,29 @@
 package com.bulletin.board.comment;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.bulletin.board.post.Post;
+import com.bulletin.board.post.PostService;
+
+import lombok.RequiredArgsConstructor;
+
+@RequestMapping("/comment")
+@RequiredArgsConstructor
+@Controller
 public class CommentController {
 
+    private final CommentService commentService;
+	private final PostService postService;
+	
+	@PostMapping("/create/{id}")
+	public String createComment(Model model, @PathVariable("id") Integer id, @RequestParam(value="content") String content) {
+		Post post = this.postService.getPost(id);
+		this.commentService.create(post, content);
+		return String.format("redirect:/post/detail/%s", id);
+	}
 }
--- BulletinBoard/src/main/java/com/bulletin/board/comment/CommentService.java
+++ BulletinBoard/src/main/java/com/bulletin/board/comment/CommentService.java
@@ -1,5 +1,23 @@
 package com.bulletin.board.comment;
 
-public class CommentService {
+import java.time.LocalDateTime;
 
+import org.springframework.stereotype.Service;
+
+import com.bulletin.board.post.Post;
+
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+@Service
+public class CommentService {
+	private final CommentRepository commentRepository;
+	
+	public void create(Post post, String content) {
+		Comment comment = new Comment();
+		comment.setContent(content);
+		comment.setCreateDate(LocalDateTime.now());
+		comment.setPost(post);
+		this.commentRepository.save(comment);
+	}
 }
