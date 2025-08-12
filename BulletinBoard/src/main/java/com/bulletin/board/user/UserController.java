8043d0fc98e0e122fe694723d55cac5e2977b0e9
nunuhelios <@LAPTOP-46K8HF6L>
2025-08-12 22:56:06 +0900

    Implement login/logout

--- BulletinBoard/src/main/java/com/bulletin/board/user/UserController.java
+++ BulletinBoard/src/main/java/com/bulletin/board/user/UserController.java
@@ -50,5 +50,10 @@ public class UserController {
         
         return "redirect:/";
     }
+    
+    @GetMapping("/login")
+    public String login() {
+        return "login_form";
+    }
 
 }
