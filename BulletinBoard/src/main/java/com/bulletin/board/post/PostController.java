dff2dcfaf51af84e2009c4859ce6019a6b7769b2
hjimin027 <hjimin027@gmail.com>
2025-08-14 13:58:00 +0900

    fix: remove unused import

--- BulletinBoard/src/main/java/com/bulletin/board/post/PostController.java
+++ BulletinBoard/src/main/java/com/bulletin/board/post/PostController.java
@@ -1,7 +1,5 @@
 package com.bulletin.board.post;
 
-import java.util.List;
-
 import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
